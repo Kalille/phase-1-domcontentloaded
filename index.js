9ac379787e3115ef56f37582f299446d185bc9ea
kalille Overton <>
2021-05-14 00:49:57 -0400

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Your code goes here
+// document.addEventListener("DOMContentLoaded", function() {
+//     console.log("The DOM has loaded");
+//   }); 
+//   console.log(
+//     "This console.log() fires when index.js loads - before DOMContentLoaded is triggered"
+//   );
+
+//   const pTag = document.getElementById('text')
+//   pTag.addEventListener('DOMContentLoaded',()=> pTag.innerHTML='This is really cool!')
+
+document.addEventListener("DOMContentLoaded",()=> {
+    const para = document.getElementById('text') 
+    para.innerHTML='This is really cool!'}
+)
